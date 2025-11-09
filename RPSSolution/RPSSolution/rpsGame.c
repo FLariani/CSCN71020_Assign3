ec7f4142e149380ba94cf3a37a1d241dfd0ed58b
FLariani <Flariani2355@conestogac.on.ca>
2025-11-09 14:22:03 -0500

    Added minimum fix for test in rpsGame.c now fiesr test passes

--- RPSSolution/RPSSolution/rpsGame.c
+++ RPSSolution/RPSSolution/rpsGame.c
@@ -1,9 +1,15 @@
 #include <stdio.h>
+#include <string.h>
 #include "rpslib.h"
 
 const char* rps_winner(const char* p1, const char* p2) {
     (void)p1; (void)p2;
-} //so it can compile
+    if (strcmp(p1, p2) == 0)
+        return "Draw";
+} 
+
+
+   
 
 
 
