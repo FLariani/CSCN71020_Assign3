211a81ef4c22dc852f92a8387ea5effcacfa118e
FLariani <Flariani2355@conestogac.on.ca>
2025-11-09 15:59:30 -0500

    Added Player1 Wins Paper Beats Rock Winning Condition to rpsGame.c

--- RPSSolution/RPSSolution/rpsGame.c
+++ RPSSolution/RPSSolution/rpsGame.c
@@ -8,10 +8,12 @@ const char* rps_winner(const char* p1, const char* p2) {
         return "Draw";
     if (strcmp(p1, "Rock") == 0 && strcmp(p2, "Scissors") == 0)
         return "Player1";
-    if (strcmp(p1, "Rock") == 0 && strcmp(p2, "Paper") == 0)
-        return "Player2";
     if (strcmp(p1, "Scissors") == 0 && strcmp(p2, "Paper") == 0)
         return "Player1";
+    if (strcmp(p1, "Paper") == 0 && strcmp(p2, "Rock") == 0)
+        return "Player1";
+    if (strcmp(p1, "Rock") == 0 && strcmp(p2, "Paper") == 0)
+        return "Player2";
 } 
 
 
